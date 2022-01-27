fa38f63fa466d16488a0af8b37f82ecd1630e00d
Mohana Deepan <mohanadeepan91@gmail.com>
2022-01-28 02:09:01 +0530

    update

--- jsfile/scrollBG.js
+++ jsfile/scrollBG.js
@@ -1,11 +1,15 @@
-const heroElem = document.querySelector(".contentBG");
-const lionElem = document.querySelector(".Homebg");
-const elephantElem = document.querySelector(".Homebg.bg2");
-
-const scrollAmount = -500;
-window.addEventListener("scroll", (event) => {
-  const { top } = heroElem.getBoundingClientRect();
-  const heroInView = top - window.innerHeight < scrollAmount;
-  lionElem.style.opacity = +!heroInView;
-  elephantElem.style.opacity = +heroInView;
+var scroll = 0;
+var conponent = document.querySelector(".imgcontainer");
+var windowHeight = window.innerHeight;
+document.addEventListener("scroll", () => {
+  scroll = window.pageYOffset;
+  if (scroll > 0 && scroll < windowHeight) {
+    //to remove all classes sauf .container
+    conponent.setAttribute("class", "imgcontainer");
+    conponent.classList.add("slide2");
+  } else if (scroll < 1 && scroll < windowHeight) {
+    //to remove all classes sauf .container
+    conponent.setAttribute("class", "imgcontainer");
+    conponent.classList.add("slide1");
+  }
 });
